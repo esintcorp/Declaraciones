5047f09db31dd76133cf474b0752428094226822
Hector Mosquera <hmosquera@carvajalonline.com>
2019-05-02 21:42:59 -0500

    Home page

--- src/view/home/Home.js
+++ src/view/home/Home.js
@@ -3,10 +3,23 @@ import React, { Component } from 'react';
 import '../../App.css';
 import Header from '../../components/logic/Header';
 import FormIconButton from '../../components/form/FormIconButton';
+import FormIconInput from '../../components/form/FormIconInput';
+
+const Item = props => <button
+  className="item"
+>
+  {props.children}
+</button>;
 
 class Home extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      user: {},
+      questions: {}
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
   }
@@ -46,22 +59,63 @@ class Home extends Component {
           // backgroundColor: 'red',
           width: '300vmin',
           float: 'left',
-          minWidth: 200
+          minWidth: 170,
+          padding: "30px 20px"
         }}>
-          <div style={{border: 'solid blue 1px', height: 40}}>IVA</div>
-          <div style={{border: 'solid blue 1px', height: 40}}>Anexos</div>
-          <div style={{border: 'solid blue 1px', height: 40}}>Renta</div>
+          <Item>IVA</Item>
+          <Item>Anexos</Item>
+          <Item>Renta</Item>
         </aside>
-          <div className="terms-div">
-            <h2><strong>Lorem Ipsum</strong></h2>
+          <div className="home-div">
+            {/* <h2><strong>Lorem Ipsum</strong></h2> */}
+            <p>Nullam eu turpis purus.</p>
+            <FormIconInput
+              // name="userId"
+              // type="email"
+              // placeholder="Correo Electrónico"
+              // value={this.state.user.userId}
+              // onChange={this.handleChange}
+              // iconName="envelope"
+              classNames={["flex1", "quest"]}
+              style={{marginTop: 0}}
+            />
+            <p>Lorem ipsum dolor sit amet</p>
+            <FormIconInput
+              // name="userId"
+              // type="email"
+              // placeholder="Correo Electrónico"
+              // value={this.state.user.userId}
+              // onChange={this.handleChange}
+              // iconName="envelope"
+              classNames={["flex1", "quest"]}
+              style={{marginTop: 0, marginBottom: 40}}
+            />
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac elit non risus congue aliquam. Nunc vitae arcu neque. Sed pellentesque at dolor sit amet gravida. Cras lorem sem, accumsan eget eros id, malesuada efficitur felis. Cras tellus urna, accumsan in velit vitae, consequat posuere erat. Phasellus ut quam sit amet lectus condimentum condimentum. Aenean sit amet erat non augue fermentum vestibulum at at diam. Aenean vitae felis sed purus pulvinar iaculis. Nullam placerat leo in hendrerit suscipit.</p>
-
-            <p>Pellentesque libero sapien, vulputate et imperdiet quis, pretium quis libero. Curabitur elementum, arcu nec pretium pulvinar, enim neque accumsan nisl, consectetur suscipit neque nunc vel augue. Sed porttitor tristique hendrerit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque vel dapibus enim. Morbi volutpat eget nisi a condimentum. Duis dictum urna ac dapibus semper. Sed a eros non dui volutpat consectetur imperdiet non elit. Maecenas ante neque, bibendum commodo fermentum sit amet, laoreet in odio. Vestibulum efficitur suscipit risus vitae pulvinar. Praesent eu metus nec tortor imperdiet auctor. Etiam aliquet euismod risus eu placerat. Aliquam eleifend leo sit amet lacus feugiat tincidunt.</p>
+            <p>Pellentesque libero sapien.</p>
+            <FormIconInput
+              // name="userId"
+              // type="email"
+              // placeholder="Correo Electrónico"
+              // value={this.state.user.userId}
+              // onChange={this.handleChange}
+              // iconName="envelope"
+              classNames={["flex1", "quest"]}
+              style={{marginTop: 0}}
+            />
 
             <p>Mauris laoreet magna eu metus iaculis dignissim. Nullam varius libero vel nulla mollis dictum. Etiam sagittis laoreet tortor ac pretium. Nam ut ipsum in ligula bibendum convallis. Aenean feugiat nibh nibh, vel consectetur sapien laoreet id. Proin vitae nunc eget arcu ultricies tristique. Donec dictum tincidunt accumsan. Curabitur in dui vel enim facilisis commodo nec sit amet urna. Morbi tincidunt, mauris vel convallis efficitur, dui nisi sollicitudin turpis, quis molestie nibh odio vitae felis. Praesent posuere in sapien nec lobortis. Ut a consequat nisi. In justo erat, fringilla a feugiat a, consequat eget tellus. Integer mauris odio, suscipit non quam vel, iaculis consectetur risus. Duis non ultrices est.</p>
+            <FormIconInput
+              // name="userId"
+              // type="email"
+              // placeholder="Correo Electrónico"
+              // value={this.state.user.userId}
+              // onChange={this.handleChange}
+              // iconName="envelope"
+              classNames={["flex1", "quest"]}
+              style={{marginTop: 0}}
+            />
 
-            <p>Mauris sed malesuada ex. Nullam eu turpis purus. Donec eu molestie enim. Vestibulum neque felis, varius ut pharetra id, consequat sit amet enim. Mauris tempor, est in tempor egestas, ante tellus semper lorem, sed venenatis dolor nibh non odio. Suspendisse ultricies faucibus dictum. Sed vulputate luctus nulla. In hac habitasse platea dictumst. Sed vitae lacus nec dolor interdum pulvinar id a elit. In placerat eget eros sit amet imperdiet. Sed consequat non purus vel pulvinar. Phasellus a sagittis purus, nec eleifend ex. Cras enim elit, venenatis id elit at, fringilla condimentum dolor. Praesent sit amet congue libero. Duis nibh libero, sollicitudin a dui vitae, tristique mollis urna.</p>
+            <p>Mauris sed malesuada ex. Donec eu molestie enim. Vestibulum neque felis, varius ut pharetra id, consequat sit amet enim. Mauris tempor, est in tempor egestas, ante tellus semper lorem, sed venenatis dolor nibh non odio. Suspendisse ultricies faucibus dictum. Sed vulputate luctus nulla. In hac habitasse platea dictumst. Sed vitae lacus nec dolor interdum pulvinar id a elit. In placerat eget eros sit amet imperdiet. Sed consequat non purus vel pulvinar. Phasellus a sagittis purus, nec eleifend ex. Cras enim elit, venenatis id elit at, fringilla condimentum dolor. Praesent sit amet congue libero. Duis nibh libero, sollicitudin a dui vitae, tristique mollis urna.</p>
 
             <p>Pellentesque augue urna, viverra vitae nisl sed, tempor pellentesque nibh. Morbi tincidunt nibh a est egestas, nec dapibus ipsum facilisis. Aenean malesuada malesuada nulla, in ornare lectus varius vitae. Proin tincidunt laoreet nunc vitae condimentum. Vestibulum eget nunc nisi. Pellentesque varius mi quis consectetur dapibus. Mauris finibus pellentesque tellus nec egestas. Suspendisse eget felis bibendum, sagittis mauris ultrices, ullamcorper ligula. In sapien elit, egestas sit amet egestas et, efficitur nec nunc. Aenean volutpat nisl nisl, a auctor nunc aliquet et. Aliquam erat volutpat. Sed vel justo diam. Nullam nec mi quis ipsum congue tristique eu non purus.</p>
 
