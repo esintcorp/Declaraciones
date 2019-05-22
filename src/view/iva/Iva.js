ee1249026237160ab01949ff0cfbe86994ce8f9e
Hector Mosquera <hmosquera@carvajalonline.com>
2019-05-22 00:55:24 +0000

    Iva queries to server.

--- src/view/iva/Iva.js
+++ src/view/iva/Iva.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react'
 
 import FormIconButton from '../../components/form/FormIconButton';
 // import FormIconInput from '../../components/form/FormIconInput';
-import { getToken } from '../../components/logic/Authentication';
 import { doFetch } from '../../utility/Util';
+
+const IvaList = props => {
+console.info('props', props)
+  return(
+    props.data ? props.data.filter(item => item.billType.id === props.filter).map(item => {
+      return (
+        <div key={item.id}>{item.name}</div>
+      )
+    }) : null
+)}
 
 class Iva extends Component {
 
@@ -11,8 +20,12 @@ class Iva extends Component {
     super(props);
 
     this.state = {
-      BillResume: {}
+      BillResume: {},
+      IvaQuestions: null,
+      IvaAnswers: [],
     };
+    // this.getIvaQuestions = this.getIvaQuestions.bind(this);
+    this.getIvaAnswers = this.getIvaAnswers.bind(this);
   }
 
   componentDidMount() {
@@ -22,10 +35,27 @@ class Iva extends Component {
         this.setState({ BillResume: data });
       }
     })
+
+    doFetch({
+      endpoint: "getIvaQuestions",
+      onOK: data2 => {
+        this.setState({ IvaQuestions: data2 });
+      }
+    })
+  }
+
+  getIvaAnswers() {
+    doFetch({
+      endpoint: "getIvaAnswers",
+      onOK: data => {
+        this.setState({ IvaAnswers: data });
+      }
+    })
   }
 
   render() {
-    const BillResume = this.state.BillResume
+    const BillResume = this.state.BillResume,
+      IvaQuestions = this.state.IvaQuestions;
 
     return <React.Fragment>
       <div className="buttons-menu">
@@ -36,14 +66,28 @@ class Iva extends Component {
           // style={{minWidth: '5vmin', margin: '0 0 0 15px', padding: 5, flex: 0}}
           iconName="list"
           // iconSize="2x"
-          onClick={this.profileFunction}
+          onClick={this.getIvaAnswers}
         />
         <button className="App-button">
           <i className='fas fa-plus fa-1x'></i>
         </button>
       </div>
       <div className="resume">
-        <div className="iva-resume">
+        <div className="iva-list">
+          <div className="iva-list-buy">
+            <IvaList
+              data={IvaQuestions}
+              filter={1}
+            />
+          </div>
+          <div className="iva-list-sell">
+            <IvaList
+              data={IvaQuestions}
+              filter={2}
+            />
+          </div>
+        </div>
+        <div className="iva-div">
           <div className="iva-resume-item">
             <div>Total Ventas:</div>
             <div className="iva-resume-item-value">
@@ -52,7 +96,7 @@ class Iva extends Component {
                   currency: 'USD',
                   // minimumFractionDigits: 0,
                   // maximumFractionDigits: 0
-              }).format(BillResume.sellTotal)}
+              }).format(BillResume.sellTotal || 0)}
             </div>
           </div>
           <div className="iva-resume-item">
@@ -63,7 +107,7 @@ class Iva extends Component {
                   currency: 'USD',
                   // minimumFractionDigits: 0,
                   // maximumFractionDigits: 0
-              }).format(BillResume.buyTotal)}
+              }).format(BillResume.buyTotal || 0)}
             </div>
           </div>
           <div className="iva-resume-item">
@@ -74,7 +118,7 @@ class Iva extends Component {
                   currency: 'USD',
                   // minimumFractionDigits: 0,
                   // maximumFractionDigits: 0
-              }).format(BillResume.paymentTotal)}
+              }).format(BillResume.paymentTotal || 0)}
             </div>
           </div>
         </div>
